e23178747219341bead75e6744264785a970f8d7
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-07-16 22:46:09 +0530

    add background img and others

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,14 +1,112 @@
+// import 'dart:js';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-      ),
-      body: Center(
-        child: Text('Welcome to the Home Screen!'),
+      body: Stack(
+        children: [
+          _buildBackgroundImage(),
+          _buildBlurOverlay(),
+          _buildTitle(),
+          _buildMenuSection(),
+        ],
       ),
     );
   }
+
+  Widget _buildBackgroundImage() {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('Images/back.jpg'), // Background image
+          fit: BoxFit.cover,
+
+        ),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: Container(
+          color: Colors.black.withOpacity(0.1),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBlurOverlay() {
+    return Container(
+      color: Colors.black.withOpacity(0.5),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        padding: EdgeInsets.only(top: 100),
+        child: Text(
+          'Welcome to My App',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget _buildMenuSection() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 120,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          children: [
+            _buildMenuIcon('assets/icon1.png', () {
+              // Add navigation logic for menu icon 1
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => Page1()),
+              // );
+            }),
+            _buildMenuIcon('assets/icon2.png', () {
+              // Add navigation logic for menu icon 2
+            }),
+            _buildMenuIcon('assets/icon3.png', () {
+              // Add navigation logic for menu icon 3
+            }),
+            _buildMenuIcon('assets/icon4.png', () {
+              // Add navigation logic for menu icon 4
+            }),
+            _buildMenuIcon('assets/icon5.png', () {
+              // Add navigation logic for menu icon 5
+            }),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMenuIcon(String imagePath, VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.all(8),
+        width: 80,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white, width: 2),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Image.asset(imagePath),
+      ),
+    );
+  }
+
 }
