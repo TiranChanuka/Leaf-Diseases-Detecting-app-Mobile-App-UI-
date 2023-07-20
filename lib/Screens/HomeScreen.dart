e741157563f64deedd9b3a96014efe4225bb288e
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-07-20 23:37:56 +0530

    Update HomeScreen.dart

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -2,6 +2,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:plant_app/Colors.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -12,7 +13,13 @@ class HomeScreen extends StatelessWidget {
           _buildBackgroundImage(),
           _buildBlurOverlay(),
           _buildTitle(),
-          _buildMenuSection(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _buildMenuSection(),
+            ),
+          )
         ],
       ),
     );
@@ -24,7 +31,6 @@ class HomeScreen extends StatelessWidget {
         image: DecorationImage(
           image: AssetImage('Images/back.jpg'), // Background image
           fit: BoxFit.cover,
-
         ),
       ),
       child: BackdropFilter(
@@ -47,58 +53,84 @@ class HomeScreen extends StatelessWidget {
       alignment: Alignment.topCenter,
       child: Container(
         padding: EdgeInsets.only(top: 100),
-        child: Text(
-          'Welcome to My App',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('Images/LogoNew.png'), // Background image
           ),
         ),
       ),
     );
   }
-
 
   Widget _buildMenuSection() {
     return Container(
       width: double.infinity,
-      height: 600,
+      height: 450,
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.white, width: 2),
-        borderRadius: BorderRadius.circular(12),
+        color: fbColor.withOpacity(0.18),
+        // backgroundBlendMode: BlendMode.overlay,
+        border: Border.all(color: Colors.white, width: 0.8),
+        borderRadius: BorderRadius.circular(15),
       ),
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: 150,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            children: [
-              _buildMenuIcon('Images/Plants/tomato.png', () {
-                // Add navigation logic for menu icon 1
-              }, 'Tomato', 'Description'),
-              _buildMenuIcon('Images/Plants/potato.png', () {
-                // Add navigation logic for menu icon 2
-              }, 'Potato', 'Description'),
-              _buildMenuIcon('Images/Plants/grapes.png', () {
-                // Add navigation logic for menu icon 3
-              }, 'Grapes', 'Description'),
-              _buildMenuIcon('assets/icon4.png', () {
-                // Add navigation logic for menu icon 4
-              }, 'Menu 1', 'Description'),
-              _buildMenuIcon('assets/icon5.png', () {
-                // Add navigation logic for menu icon 5
-              }, 'Menu 1', 'Description'),
-            ],
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // Align children to the left
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Text(
+              '\nMenu Section\n', // Your desired topic for the section
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
           ),
-        ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada ex nec mauris faucibus, id ullamcorper ipsum cursus. Nunc vitae ligula ac ipsum semper sollicitudin. In sodales, sapien sit amet egestas luctus\nfelis nisl interdum ex, at rhoncus velit ligula ac velit. Vivamus eget fermentum ex.\n',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                children: [
+                  _buildMenuIcon('Images/Plants/tomato.png', () {
+                    // Add navigation logic for menu icon 1
+                  }, 'Tomato', 'Description'),
+                  _buildMenuIcon('Images/Plants/potato.png', () {
+                    // Add navigation logic for menu icon 2
+                  }, 'Potato', 'Description'),
+                  _buildMenuIcon('Images/Plants/grapes.png', () {
+                    // Add navigation logic for menu icon 3
+                  }, 'Grapes', 'Description'),
+                  _buildMenuIcon('assets/icon4.png', () {
+                    // Add navigation logic for menu icon 4
+                  }, 'Tea', 'Description'),
+                  _buildMenuIcon('assets/icon5.png', () {
+                    // Add navigation logic for menu icon 5
+                  }, 'Cone', 'Description'),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  Widget _buildMenuIcon(String imagePath, VoidCallback onTap, String title, String additionalText) {
+  Widget _buildMenuIcon(String imagePath, VoidCallback onTap, String title,
+      String additionalText) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
@@ -121,13 +153,9 @@ class HomeScreen extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-
           ],
         ),
       ),
     );
   }
-
-
-
 }
