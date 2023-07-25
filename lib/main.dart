b6d6ad29db8508d53ee94ca39dcbced4a2b055b4
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-07-25 23:13:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import './Screens/LandingScreen.dart';
 
-void main()=>runApp(MyApp()
+
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
 );
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -10,6 +19,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       home:PlantLandingPage(),
     );
