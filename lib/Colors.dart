221f96976a2fe08158bdf38ad077fbb5c8543262
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-09-07 01:28:34 +0530

    Update Colors.dart

--- lib/Colors.dart
+++ lib/Colors.dart
@@ -5,3 +5,4 @@ const Color fColor=Color(0xFF000000);
 const Color fbColor=Color(0xDADADAFF);
 const Color sideMenuColor=Color(0xFFDBE1DB);
 const Color rColor=Color(0x5222752E);
+const Color buttonColor=Color(0xFF8AD167);
