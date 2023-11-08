de7e0f904a79ea6ffb79cc6cedebff4d53343e41
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-11-08 23:03:24 +0530

    Update ScanPageTomato.dart

--- lib/ScanScreens/ScanPageTomato.dart
+++ lib/ScanScreens/ScanPageTomato.dart
@@ -198,6 +198,7 @@ class _ImagePickerPageState extends State<ImagePickerPageTomato> {
                 ),
               ),
               SizedBox(height: 35),
+
             ],
           ),
         ],
